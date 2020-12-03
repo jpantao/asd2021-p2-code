ead2d4efb0367cbcddef1980c01046d48edb7737
João <jp.antao@campus.fct.unl.pt>
2020-12-03 11:58:53 +0000

    RedirectMessage added

---
+++ src/main/java/protocols/statemachine/messages/RedirectMessage.java
@@ -0,0 +1,42 @@
+package protocols.statemachine.messages;
+
+import io.netty.buffer.ByteBuf;
+import pt.unl.fct.di.novasys.babel.generic.ProtoMessage;
+import pt.unl.fct.di.novasys.network.ISerializer;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class RedirectMessage extends ProtoMessage {
+    public static final short MSG_ID = 202;
+
+    private final byte[] operation;
+
+    public RedirectMessage(byte[] operation) {
+        super(MSG_ID);
+        this.operation = operation;
+    }
+
+    public byte[] getOperation() {
+        return operation;
+    }
+
+    @Override
+    public String toString() {
+        return "RedirectMessage{" +
+                "operation=" + Arrays.toString(operation) +
+                '}';
+    }
+
+    public static ISerializer<RedirectMessage> serializer = new ISerializer<RedirectMessage>() {
+        @Override
+        public void serialize(RedirectMessage msg, ByteBuf buf) throws IOException {
+            buf.writeBytes(msg.operation);
+        }
+
+        @Override
+        public RedirectMessage deserialize(ByteBuf buf) throws IOException {
+            return new RedirectMessage(buf.array());
+        }
+    };
+}
